099061f1d80b1b2b0a0856144a84ff3bf728c7f2
negu63 <dnjsdn2019@gmail.com>
2021-12-20 16:38:29 +0900

    solve: practice 6.1

--- code-ch06/op.py
+++ code-ch06/op.py
@@ -717,6 +717,12 @@ def op_checksequenceverify(stack, version, sequence):
             return False
     return True
 
+def op_hash160(stack):
+    if len(stack) < 1:
+        return False
+    element = stack.pop()
+    stack.append(hash160(element))
+    return True
 
 class OpTest(TestCase):
 
