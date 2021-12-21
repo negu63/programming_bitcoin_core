6dd8302f91f32f594b2f7c0abb879bf82d4cec27
negu63 <dnjsdn2019@gmail.com>
2021-12-22 02:50:56 +0900

    solve: practice 7.1

--- code-ch07/tx.py
+++ code-ch07/tx.py
@@ -174,8 +174,30 @@ class Tx:
         # add SIGHASH_ALL using int_to_little_endian in 4 bytes
         # hash256 the serialization
         # convert the result to an integer using int.from_bytes(x, 'big')
-        raise NotImplementedError
-
+        s = int_to_little_endian(self.version, 4)
+        s += encode_varint(len(self.tx_ins))
+        for i, tx_in in enumerate(self.tx_ins):
+            if i == input_index:
+                s += TxIn(
+                    prev_tx=tx_in.prev_tx, 
+                    prev_index=tx_in.prev_index, 
+                    script_sig=tx_in.script_pubkey(self.testnet),
+                    sequence=tx_in.sequence
+                ).serialize()
+            else:
+                s += TxIn(
+                    prev_tx=tx_in.prev_tx, 
+                    prev_index=tx_in.prev_index, 
+                    sequence=tx_in.sequence
+                ).serialize()
+        s += encode_varint(len(self.tx_outs))
+        for tx_out in self.tx_outs:
+            s += tx_out.serialize()
+        s += int_to_little_endian(self.locktime, 4)
+        s += int_to_little_endian(SIGHASH_ALL, 4)
+        h256 = hash256(s)
+        return int.from_bytes(h256, 'big')
+                
     def verify_input(self, input_index):
         '''Returns whether the input has a valid signature'''
         # get the relevant input
