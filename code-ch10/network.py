944cb404e6d4a0cb2571b5d9a8e5a9e153838b4b
negu63 <dnjsdn2019@gmail.com>
2022-01-01 23:46:22 +0900

    solve: practice 10.5

--- code-ch10/network.py
+++ code-ch10/network.py
@@ -310,9 +310,12 @@ class SimpleNode:
         '''Do a handshake with the other node.
         Handshake is sending a version message and getting a verack back.'''
         # create a version message
+        version = VersionMessage()
         # send the command
+        self.send(version)
         # wait for a verack message
-        raise NotImplementedError
+        self.wait_for(VerAckMessage)
+
     # tag::source4[]
 
     def send(self, message):  # <1>
